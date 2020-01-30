9ba1c94ce9dc31e45c7872611f208bfcaa16b2cf
Carl Eriksson <carl.erikssonn@hotmail.com>
2020-01-30 09:30:44 +0100

    Carl gjorde cube controller

---
+++ Unity Project/Assets/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    MeshRenderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        Color lerpColor = Color.Lerp(Color.blue, Color.red, 0.1f);
+        rend.material.color = lerpColor;
+    }
+}
